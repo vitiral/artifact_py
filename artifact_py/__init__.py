51594d305ff301c9f5281de8c8e1ce1aad4afb52
Rett Berg <rett@google.com>
2019-07-19 15:53:00 -0700

    add license header

--- artifact_py/__init__.py
+++ artifact_py/__init__.py
@@ -1,3 +1,19 @@
+# artifact_py: the design documentation tool made for everyone.
+#
+# Copyright (C) 2019 Rett Berg <github.com/vitiral>
+#
+# The source code is Licensed under either of
+#
+# * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+#   http://www.apache.org/licenses/LICENSE-2.0)
+# * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+#   http://opensource.org/licenses/MIT)
+#
+# at your option.
+#
+# Unless you explicitly state otherwise, any contribution intentionally submitted
+# for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+# be dual licensed as above, without any additional terms or conditions.
 from __future__ import print_function, unicode_literals
 """Python library for artifact."""
 
--- artifact_py/settings.py
+++ artifact_py/settings.py
@@ -1,3 +1,19 @@
+# artifact_py: the design documentation tool made for everyone.
+#
+# Copyright (C) 2019 Rett Berg <github.com/vitiral>
+#
+# The source code is Licensed under either of
+#
+# * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+#   http://www.apache.org/licenses/LICENSE-2.0)
+# * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+#   http://opensource.org/licenses/MIT)
+#
+# at your option.
+#
+# Unless you explicitly state otherwise, any contribution intentionally submitted
+# for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+# be dual licensed as above, without any additional terms or conditions.
 from __future__ import unicode_literals
 import os
 import six
@@ -39,6 +55,9 @@ class Settings:
     def relpath_all(self, paths):
         """Return the relative paths to the root directory."""
         return [self.relpath(p) for p in paths]
+
+    def serialize_list(self, lst):
+        return [v.serialize(self) for v in lst]
 
     def serialize_list(self, lst):
         return [v.serialize(self) for v in lst]
