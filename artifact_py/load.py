d9a5bfc7e26b361208ca29b9f38740e1f4a21691
Rett Berg <rett@google.com>
2019-07-19 23:10:51 -0700

    fix py2 division issue

--- artifact_py/load.py
+++ artifact_py/load.py
@@ -14,7 +14,7 @@
 # Unless you explicitly state otherwise, any contribution intentionally submitted
 # for inclusion in the work by you, as defined in the Apache-2.0 license, shall
 # be dual licensed as above, without any additional terms or conditions.
-from __future__ import unicode_literals
+from __future__ import unicode_literals, division
 import copy
 
 import six
